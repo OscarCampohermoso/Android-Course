ac8f986720cc2d6af96027569190b9df139ebc3f
OscarCampohermoso <oscarcampohermoso8@gmail.com>
2023-02-10 23:20:03 -0400

    Clase 3 con try and catch

--- Clase3Civi1/app/src/main/java/com/example/clase3civi1/MainActivity.java
+++ Clase3Civi1/app/src/main/java/com/example/clase3civi1/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -22,25 +23,71 @@ public class MainActivity extends AppCompatActivity {
         etclave = findViewById(R.id.etClave);
         etnombre = findViewById(R.id.etNombre);
 
+        Log.d("Ciclo de vida", "onCreate() dentro de A1");
 
         tvinvocaa2 = findViewById(R.id.tvInvocaA2);
 
         tvinvocaa2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String clave = etclave.getText().toString().trim();
-                String nombre = etnombre.getText().toString().trim();
-                if (clave.length() > 0 && nombre.length() > 0){
+//                String clave = etclave.getText().toString().trim();
+//                String nombre = etnombre.getText().toString().trim();
+//                if (clave.length() > 0 && nombre.length() > 0){
+//                    bolsa.putInt("clave", Integer.parseInt(etclave.getText().toString()));
+//                    bolsa.putString("nombre", etnombre.getText().toString());
+//                    Intent invocador = new Intent(MainActivity.this, MainActivity2.class);
+//                    invocador.putExtras(bolsa);
+//                    startActivity(invocador);
+//                } else{
+//                    Toast.makeText(MainActivity.this, "Faltan datos", Toast.LENGTH_LONG).show();
+//                }
+                try{
                     bolsa.putInt("clave", Integer.parseInt(etclave.getText().toString()));
                     bolsa.putString("nombre", etnombre.getText().toString());
                     Intent invocador = new Intent(MainActivity.this, MainActivity2.class);
                     invocador.putExtras(bolsa);
                     startActivity(invocador);
-
-                } else{
-                    Toast.makeText(MainActivity.this, "Faltan datos", Toast.LENGTH_SHORT).show();
+                }catch(Exception e){
+                    Toast.makeText(MainActivity.this, "Faltan datos: " + e.getMessage(), Toast.LENGTH_LONG).show();
                 }
             }
         });
     }
+
+    // Ciclo de vida de un activity
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("Ciclo de vida", "onStart() dentro de A1");
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("Ciclo de vida", "onResume() dentro de A1");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("Ciclo de vida", "onPause() dentro de A1");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("Ciclo de vida", "onStop() dentro de A1");
+    }
+
+//    @Override
+//    protected void onDestroy() {
+//        super.onDestroy();
+//        Toast.makeText(this, "onDestroy", Toast.LENGTH_SHORT).show();
+//    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("Ciclo de vida", "onRestart() dentro de A1");
+    }
+
 }
--- Clase3Civi1/app/src/main/java/com/example/clase3civi1/MainActivity2.java
+++ Clase3Civi1/app/src/main/java/com/example/clase3civi1/MainActivity2.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -21,13 +22,52 @@ public class MainActivity2 extends AppCompatActivity {
         tvvolvera1 = findViewById(R.id.tvVolverA1);
         Bundle bolsa = getIntent().getExtras();
 
+        Log.d("Ciclo de vida", "onCreate() dentro de A2");
+
+
         tvrecibido.setText("Clave: " + String.valueOf(bolsa.getInt("clave"))
-                            + " Nombre: " + bolsa.getString("nombre"));
+                            + " \nNombre: " + bolsa.getString("nombre"));
         tvvolvera1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
             }
         });
+    }
+
+    // Ciclo de vida de un activity
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("Ciclo de vida", "onStart() dentro de A2");
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("Ciclo de vida", "onResume() dentro de A2");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("Ciclo de vida", "onPause() dentro de A2");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("Ciclo de vida", "onStop() dentro de A2");
+    }
+
+//    @Override
+//    protected void onDestroy() {
+//        super.onDestroy();
+//        Toast.makeText(this, "onDestroy", Toast.LENGTH_SHORT).show();
+//    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("Ciclo de vida", "onRestart() dentro de A2");
     }
 }
